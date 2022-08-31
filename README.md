92bbe501ad06128163cb1f44a9787b407b420d81
Mitchell Humphries <mtharse@gmail.com>
2022-08-30 23:22:30 -0400

    Submitted PhotoGame UE5 project
    Added project to GitHub

---
+++ README.md
@@ -0,0 +1,3 @@
+# PhotoGame
+
+Developed with Unreal Engine 5
---
+++ Source/PhotoGame/PhotoGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PhotoGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PhotoGame, "PhotoGame" );
